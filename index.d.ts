b4c925a234d9a8000f6da005cee0656450193f33
Timothy Jones <timothy.l.jones@gmail.com>
2021-08-13 00:10:56 +1000

    chore: Add new property to typescript types

--- index.d.ts
+++ index.d.ts
@@ -14,6 +14,7 @@ type GitInfo = {
 type GitDescribeOptions = {
   dirtyMark?: string;
   dirtySemver?: boolean;
+  long?: boolean;
   longSemver?: boolean;
   requireAnnotated?: boolean;
   match?: string;
